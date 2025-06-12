e70b64b8e0604d230959737eddf17f4d89273a2f
Success Olugbenga <preshdivine372@gmail.com>
2025-06-12 02:10:19 -0700

    media query adjustment

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
- var swiper = new Swiper(".heroSwiper", {
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.getElementById('menu_btn');
+    const closeBtn = document.getElementById('close_btn');
+    const menu = document.getElementById('menu');
+
+    menuBtn.addEventListener('click', () => {
+      menu.classList.add('active');
+    });
+
+    closeBtn.addEventListener('click', () => {
+      menu.classList.remove('active');
+    });
+
+    // Swiper Initialization
+    var swiper = new Swiper(".heroSwiper", {
       spaceBetween: 30,
       centeredSlides: true,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
       },
-      loop:true
-      
+      loop: true
     });
-
 
     var swiper2 = new Swiper(".heroSwiper2", {
       spaceBetween: 30,
@@ -17,21 +29,7 @@
         delay: 3000,
         disableOnInteraction: false,
       },
-      loop:true
-      
+      loop: true
     });
-
-
-
-   const menuBtn = document.getElementById('menu_btn');
-  const closeBtn = document.getElementById('close_btn');
-  const menu = document.getElementById('menu');
-
-  menuBtn.addEventListener('click', () => {
-    menu.classList.add('active');
-  });
-
-  closeBtn.addEventListener('click', () => {
-    menu.classList.remove('active');
   });
 
